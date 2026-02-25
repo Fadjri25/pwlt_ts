cdc529d2ec59ab10942c1f4e10162073e96e1fb4
Fadjri25 <h1101241060@student.untan.ac.id>
2026-02-25 04:49:26 +0000

    update mahasiswa

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ function hello(nama :string, umur:number):string{
     return `selamat datang ${nama} di PWL, Umur ${umur}`;
 
 }
-const mahasiswa="Audy";
+const mahasiswa="Fadjri";
 const umur:number=19;
 console.log(hello(mahasiswa,umur));
