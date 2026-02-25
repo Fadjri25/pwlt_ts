d8f0a11230c1a7468c580024ce4f90d14317773f
Fadjri25 <h1101241060@student.untan.ac.id>
2026-02-25 04:42:08 +0000

    update.index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama) {
+    return `selamat datang ${nama} di PWL`;
+    +nama;
+}
+console.log(hello('jony'));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function hello(nama :string, umur:number):string{
+    return `selamat datang ${nama} di PWL, Umur ${umur}`;
+
+}
+const mahasiswa="Audy";
+const umur:number=19;
+console.log(hello(mahasiswa,umur));
